37f207ab7080bfdea9edd2ffc6e189527b9eec29
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2025-03-02 12:35:09 +0100

    wip

--- src/language_servers/language_server.rs
+++ src/language_servers/language_server.rs
@@ -6,6 +6,7 @@ use zed_extension_api::{
 pub struct LanguageServerBinary {
     pub path: String,
     pub args: Option<Vec<String>>,
+    pub env: Option<Vec<(String, String)>>,
 }
 
 pub trait LanguageServer {
@@ -28,10 +29,12 @@ pub trait LanguageServer {
     ) -> Result<zed::Command> {
         let binary = self.language_server_binary(language_server_id, worktree)?;
 
+        dbg!(binary.env.clone());
+
         Ok(zed::Command {
             command: binary.path,
             args: binary.args.unwrap_or(Self::get_executable_args()),
-            env: Default::default(),
+            env: binary.env.clone().unwrap_or_default(),
         })
     }
 
@@ -40,11 +43,11 @@ pub trait LanguageServer {
         language_server_id: &LanguageServerId,
         worktree: &zed::Worktree,
     ) -> Result<LanguageServerBinary> {
-        let output = Command::new("ls").output()?;
+        // let output = Command::new("ls").output()?;
 
-        dbg!(&output.status);
-        dbg!(String::from_utf8_lossy(&output.stdout).to_string());
-        dbg!(String::from_utf8_lossy(&output.stderr).to_string());
+        // dbg!(&output.status);
+        // dbg!(String::from_utf8_lossy(&output.stdout).to_string());
+        // dbg!(String::from_utf8_lossy(&output.stderr).to_string());
 
         let lsp_settings = LspSettings::for_worktree(language_server_id.as_ref(), worktree)?;
 
@@ -53,6 +56,7 @@ pub trait LanguageServer {
                 return Ok(LanguageServerBinary {
                     path,
                     args: binary_settings.arguments,
+                    env: Default::default(),
                 });
             }
         }
@@ -63,29 +67,63 @@ pub trait LanguageServer {
             .and_then(|settings| settings["use_bundler"].as_bool())
             .unwrap_or(Self::default_use_bundler());
 
-        if use_bundler {
-            worktree
-                .which("bundle")
-                .map(|path| LanguageServerBinary {
-                    path,
-                    args: Some(
-                        [
-                            vec!["exec".to_string(), Self::EXECUTABLE_NAME.to_string()],
-                            Self::get_executable_args(),
-                        ]
-                        .concat(),
-                    ),
-                })
-                .ok_or_else(|| "Unable to find the 'bundle' command.".into())
-        } else {
-            worktree
-                .which(Self::EXECUTABLE_NAME)
-                .map(|path| LanguageServerBinary {
-                    path,
-                    args: Some(Self::get_executable_args()),
-                })
-                .ok_or_else(|| format!("Unable to find the '{}' command.", Self::EXECUTABLE_NAME))
-        }
+        // if use_bundler {
+        //     worktree
+        //         .which("bundle")
+        //         .map(|path| LanguageServerBinary {
+        //             path,
+        //             args: Some(
+        //                 [
+        //                     vec!["exec".to_string(), Self::EXECUTABLE_NAME.to_string()],
+        //                     Self::get_executable_args(),
+        //                 ]
+        //                 .concat(),
+        //             ),
+        //             env: Default::default(),
+        //         })
+        //         .ok_or_else(|| "Unable to find the 'bundle' command.".into())
+        // } else {
+        let current_directory = std::env::current_dir()
+            .map_err(|e| format!("Failed to get current directory: {}", e))?
+            .to_string_lossy()
+            .to_string();
+
+        dbg!(&current_directory);
+
+        let output = Command::new("gem")
+            .env("GEM_HOME", current_directory.clone())
+            .arg("install")
+            .arg("--no-user-install")
+            .arg("--no-format-executable")
+            .arg("--no-document")
+            .arg(Self::GEM_NAME)
+            .output()?;
+
+        dbg!(String::from_utf8_lossy(&output.stdout).to_string());
+        dbg!(String::from_utf8_lossy(&output.stderr).to_string());
+
+        // if output.status == 0 {}
+        return Ok(LanguageServerBinary {
+            path: format!("{}/bin/solargraph", current_directory),
+            args: Some(Self::get_executable_args()),
+            env: Some(vec![(
+                "GEM_PATH".to_string(),
+                format!("{gem_path}:$GEM_PATH", gem_path = current_directory),
+            )]),
+        });
+
+        // worktree
+        //     .which(Self::EXECUTABLE_NAME)
+        //     .map(|path| LanguageServerBinary {
+        //         path,
+        //         args: Some(Self::get_executable_args()),
+        //         env: Some(vec![
+        //             ("GEM_HOME".to_string(), current_directory.clone()),
+        //             ("GEM_PATH".to_string(), current_directory),
+        //         ]),
+        //     })
+        //     .ok_or_else(|| format!("Unable to find the '{}' command.", Self::EXECUTABLE_NAME))
+        // }
     }
 }
 
